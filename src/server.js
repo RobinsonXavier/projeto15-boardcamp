446eec86933c1efd9fee799bb511b788b6dc3555
Robinson_Xavier <robinson.webzen@gmail.com>
2022-10-03 04:41:46 -0300

    added get and post functions of games

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pg from 'pg';
+import joi from 'joi';
 import dotenv from 'dotenv';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
@@ -10,6 +11,14 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({
     path: path.resolve(__dirname, '../.env')
+});
+
+const gamesSchema = joi.object({
+    name: joi.string().required(),
+    image: joi.string(),
+    stockTotal: joi.number().required().min(1),
+    pricePerDay: joi.number().required().min(1),
+    categoryId: joi.number().required()
 });
 
 const {Pool} = pg;
@@ -24,7 +33,6 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/categories', (req, res) => {
-    const { name } = req.body;
 
     connection.query('SELECT * FROM categories;').then(response => {
         res.status(200).send(response.rows)
@@ -47,6 +55,55 @@ app.post('/categories', async (req, res) => {
     }
 
     connection.query('INSERT INTO categories (name) VALUES ($1);', [name]).then(response => {
+        res.sendStatus(201);
+    });
+});
+
+app.get('/games', (req, res) => {
+    const { name } = req.query;
+
+    if(name) {
+        connection.query(`SELECT games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id WHERE LOWER(SUBSTRING(games.name, 1, ${name.length})) = $1;`,[name.toLowerCase()]).then(response => {
+            return res.status(200).send(response.rows);
+        });
+    }
+
+    connection.query(`SELECT games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id;`).then(response => {
+        res.status(200).send(response.rows)
+    });
+});
+
+app.post('/games', async (req, res) => {
+    const {name, image, stockTotal, categoryId, pricePerDay} = req.body;
+
+    const validation = gamesSchema.validate(req.body, {abortEarly: false});
+
+    if (validation.error) {
+        const errors = validation.error.details.map (detail => detail.message);
+        return res.status(400).send(errors); 
+    }
+
+    const categories = await connection.query('SELECT * FROM categories;');
+
+    const haveId = categories.rows.find(element => {
+        return element.id === categoryId
+    });
+
+    if(!haveId) {
+        return res.sendStatus(400);
+    }
+
+    const getGames = await connection.query('SELECT * FROM games;');
+
+    const sameName = getGames.rows.find(element => {
+        return element.name === name;
+    });
+
+    if (sameName) {
+        return res.sendStatus(409);
+    }
+
+    connection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);`, [ name, image, stockTotal, categoryId, pricePerDay]).then(response => {
         res.sendStatus(201);
     });
 });
